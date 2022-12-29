cd98f26bda3196f313349d13132a54f1dd52391a
Mojeeto <mojtaba01nouri@gmail.com>
2022-12-29 11:36:39 +0330

    add user apis and authentication

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
+    'user.apps.UserConfig',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
 ]
 
